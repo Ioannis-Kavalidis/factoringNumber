928a4b1fc90a80aa7a07dea83c9e11aedb6ecdb4
Ioannis Kavalidis <dreamtnem@gmail.com>
2020-02-18 14:47:30 +0100

    Create secondSolution.js

---
+++ secondSolution.js
@@ -0,0 +1,11 @@
+
+// with a FOR loop
+
+function factoring2(num) {
+  if (num === 0 || num === 1) return 1;
+  for (var i = num - 1; i >= 1; i--) {
+    num *= i;
+  }
+  return num;
+}
+console.log(factoring2(5));
